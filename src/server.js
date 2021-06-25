9e8ff77fa0c06918529160d9af318628e813e035
alexercolin <alex_ercolin@hotmail.com>
2021-06-25 16:45:56 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const route = require('./route');
+const path = require('path');
+
+const server = express();
+
+// Basicamente, aqui eu falo as pastas que o meu servidor tem que usar
+server.set('view engine', 'ejs');
+
+server.use(express.static("public"));
+
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(route);
+
+server.listen(3000, () => console.log('Rodando'));
