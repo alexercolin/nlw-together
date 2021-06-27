cdc759e956f9751e8c21399a58f19d896f0a0104
alexercolin <alex_ercolin@hotmail.com>
2021-06-27 19:18:05 -0300

    Adiciona new features

--- src/route.js
+++ src/route.js
@@ -7,11 +7,13 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req,res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room/:room', (req,res) => res.render("room"))
-
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
 
 // Formato que o form dentro da modal tem que passar a info
 route.post('/question/:room/:question/:action', QuestionController.index)
 route.post('/create-room', RoomController.create)
+
+route.post('/questions/create/:room', QuestionController.create)
 
 module.exports = route
